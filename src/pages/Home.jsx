f83e88ae1a6bdd0da476e433086ecb1f4fe60712
Pradeep Singh <singhpardeep585@gmail.com>
2022-12-19 18:22:39 +0530

    create About section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { Button, Heading, SubHeading, Paragraph } from "../utils/styledComponents/components";
+import {
+  Button,
+  Heading,
+  SubHeading,
+  Paragraph,
+} from "../utils/styledComponents/components";
 import img from "../images/demoimg.jpg";
 
 const Home = () => {
@@ -9,13 +14,29 @@ const Home = () => {
       <main className="homePage">
         {/* hero section*/}
         {/* # section 1 */}
-        <section className="heroSection min-h-[500px] bg-primary flex">
+        <section className="heroSection min-h-[500px] bg-primary flex ">
           <div className="container flex mx-auto items-center">
             {/* left cloumn */}
             <div className="left w-1/2">
-              <Heading className="mb-1" fontSize={'36px'} color={'var(--white)'}>Lorem ipsum dolor</Heading>
-              <SubHeading fontStyle={'italic'} bgColor={'var(--black)'} display={'inline'} color={'var(--white)'} letterSpacing={'1px'}>Lorem ipsum dolor sit, amet consectetur.</SubHeading>
-              <Paragraph className="mt-4" color={'var(--white)'}>
+              <Heading
+                className="mb-1"
+                fontSize={"36px"}
+                color={"var(--white)"}
+              >
+                Lorem ipsum dolor
+              </Heading>
+              <SubHeading
+                fontStyle={"italic"}
+                color={"var(--white)"}
+                letterSpacing={"1px"}
+              >
+                <mark
+                  style={{ background: "var(--black)", color: "var(--white)" }}
+                >
+                  Lorem ipsum dolor sit, amet consectetur.
+                </mark>
+              </SubHeading>
+              <Paragraph className="mt-4" color={"var(--white)"}>
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi
                 obcaecati voluptas aut sint, culpa beatae ratione id maxime
                 debitis officiis.
@@ -30,7 +51,7 @@ const Home = () => {
                   borderRadius="5px"
                   hoverBgColor="var(--primary)"
                   hoverColor="var(--white)"
-                  hoverBorder = "1px solid var(--white)"
+                  hoverBorder="1px solid var(--white)"
                 >
                   Shop Now
                 </Button>
@@ -52,9 +73,52 @@ const Home = () => {
                 <img src={img} alt="" className="h-40 object-cover rounded" />
                 <img src={img} alt="" className="h-40 object-cover rounded" />
                 <div className="col-span-2">
-                <img src={img} alt="" className="w-full h-40 object-cover rounded"/>
+                  <img
+                    src={img}
+                    alt=""
+                    className="w-full h-40 object-cover rounded"
+                  />
                 </div>
               </div>
+            </div>
+          </div>
+        </section>
+
+        {/* About Section */}
+        {/* # Section-2 */}
+        <section className="aboutSection bg-white">
+          {/* section container */}
+          {/* About summary section */}
+          <div className="container w-full md:w-8/12 mx-auto text-black">
+            {/* sub heading div */}
+            <div className="aboutSummary flex flex-col text-center p-4 md:py-12 md:px-8 bg-[#fff] md:shadow-xl hover:md:shadow-2xl md:rounded-2xl relative bottom-12 gap-3">
+              <SubHeading className="capitalize">Sub Heading</SubHeading>
+              <Paragraph>
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                Corrupti at cum corporis praesentium delectus doloremque maiores
+                similique. Non, quod perferendis voluptates dolorum quos,
+                recusandae fugiat a deserunt in optio sapiente?
+              </Paragraph>
+            </div>
+          </div>
+
+          {/* About us container */}
+          <div className="container flex flex-col md:flex-row mx-auto px-4 gap-16 mt-14 items-center">
+            <div className="left-col w-full md:w-1/2">
+              <img src={img} alt="" className="w-full md:h-[380px] object-cover"/>
+            </div>
+            <div className="right-col  w-full md:w-1/2">
+              <SubHeading className="mb-5" fontSize={'18px'} fontWeight={'500'} textDecoration={'underline'} opacity={'.7'}>About Us</SubHeading>
+              <Heading fontSize={'32px'} fontWeight={'600'} fontStyle={'italic'} letterSpacing={'1px'}>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              </Heading>
+              <Paragraph className="mt-7">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla
+                tempora deserunt laborum sint aliquam magni molestiae maxime,
+                praesentium totam in, ullam unde odio quasi autem amet eius
+                numquam suscipit laudantium.
+              </Paragraph>
+              <Button className="mt-6">Contact</Button>
             </div>
           </div>
         </section>
--- src/utils/styledComponents/components.jsx
+++ src/utils/styledComponents/components.jsx
@@ -4,6 +4,7 @@ import styled from "styled-components";
 export const Button = styled.button(
   ({
     color,
+    opacity,
     bgColor,
     border,
     borderRadius,
@@ -13,62 +14,111 @@ export const Button = styled.button(
     size,
     width,
     height,
-    fontFamily
+    fontFamily,
   }) => ({
-    color: color,
-    backgroundColor: bgColor,
-    border: border,
-    borderRadius: borderRadius,
-    fontSize: size,
-    width: width,
-    height: height,
-    fontFamily: fontFamily,
+    color: (color || '#fff'),
+    opacity: (opacity || '1'),
+    backgroundColor: (bgColor || '#000'),
+    border: (border || ''),
+    borderRadius: (borderRadius || '4px'),
+    fontSize: (size || ''),
+    width: (width || '100px'),
+    height: (height || '40px'),
+    fontFamily: (fontFamily || ''),
     "&:hover": {
       color: hoverColor,
       border: hoverBorder,
       backgroundColor: hoverBgColor,
-    }
+    },
   })
 );
 
 export const Heading = styled.h1(
-  ({fontWeight, fontFamily, fontSize, fontStyle, letterSpacing, lineHeight, lineBreak, color, bgColor, display})=>({
-  fontSize:  (fontSize || '42px'),
-  fontFamily: (fontFamily || ''),
-  fontWeight: (fontWeight || 700),
-  fontStyle: (fontStyle || 'normal'),
-  letterSpacing:  (letterSpacing || 'normal'),
-  lineHeight: (lineHeight || 'normal'),
-  lineBreak: (lineBreak || 'auto'),
-  color: (color || ''),
-  backgroundColor: (bgColor || ''),
-  display : (display || ''),
-})
+  ({
+    fontWeight,
+    fontFamily,
+    fontSize,
+    fontStyle,
+    letterSpacing,
+    lineHeight,
+    lineBreak,
+    color,
+    opacity,
+    bgColor,
+    display,
+    textDecoration,
+  }) => ({
+    fontSize: fontSize || "42px",
+    fontFamily: fontFamily || "",
+    fontWeight: fontWeight || 700,
+    fontStyle: fontStyle || "normal",
+    letterSpacing: letterSpacing || "normal",
+    lineHeight: lineHeight || "normal",
+    lineBreak: lineBreak || "auto",
+    color: color || "",
+    opacity: (opacity || '1'),
+    backgroundColor: bgColor || "",
+    display: display || "",
+    textDecoration: textDecoration || "",
+  })
 );
 
-export const SubHeading = styled.h2(({fontWeight, fontFamily, fontSize, fontStyle, letterSpacing, lineHeight, lineBreak, color, bgColor, display})=>({
-  fontSize: (fontSize || '22px'),
-  fontFamily: (fontFamily || ''),
-  fontWeight: (fontWeight || 500),
-  fontStyle: (fontStyle || 'normal'),
-  letterSpacing:  (letterSpacing || 'normal'),
-  lineHeight: (lineHeight || 'normal'),
-  lineBreak: (lineBreak || 'auto'),
-  color: (color || ''),
-  backgroundColor: (bgColor || ''),
-  display : (display || ''),
-}))
+export const SubHeading = styled.h2(
+  ({
+    fontWeight,
+    fontFamily,
+    fontSize,
+    fontStyle,
+    letterSpacing,
+    lineHeight,
+    lineBreak,
+    color,
+    opacity,
+    bgColor,
+    display,
+    textDecoration,
+  }) => ({
+    fontSize: fontSize || "22px",
+    fontFamily: fontFamily || "",
+    fontWeight: fontWeight || 500,
+    fontStyle: fontStyle || "normal",
+    letterSpacing: letterSpacing || "normal",
+    lineHeight: lineHeight || "normal",
+    lineBreak: lineBreak || "auto",
+    color: color || "",
+    opacity: (opacity || '1'),
+    backgroundColor: bgColor || "",
+    display: display || "",
+    textDecoration: textDecoration || "",
+  })
+);
 
-
-export const Paragraph = styled.p(({fontWeight, fontFamily, fontSize, fontStyle, letterSpacing, lineHeight, lineBreak, color, bgColor, display})=>({
-  fontSize: (fontSize || '16px'),
-  fontFamily: (fontFamily || ''),
-  fontWeight: (fontWeight || 400),
-  fontStyle: (fontStyle || 'normal'),
-  letterSpacing:  (letterSpacing || 'normal'),
-  lineHeight: (lineHeight || 'normal'),
-  lineBreak: (lineBreak || 'auto'),
-  color: (color || ''),
-  backgroundColor: (bgColor || ''),
-  display : (display || ''),
-}))
+export const Paragraph = styled.p(
+  ({
+    fontWeight,
+    fontFamily,
+    fontSize,
+    fontStyle,
+    letterSpacing,
+    lineHeight,
+    lineBreak,
+    color,
+    opacity,
+    bgColor,
+    display,
+    textDecoration,
+  }) => ({
+    fontSize: fontSize || "16px",
+    fontFamily: fontFamily || "",
+    fontWeight: fontWeight || 400,
+    fontStyle: fontStyle || "normal",
+    letterSpacing: letterSpacing || "normal",
+    lineHeight: lineHeight || "normal",
+    lineBreak: lineBreak || "auto",
+    color: color || "",
+    opacity: (opacity || '1'),
+    backgroundColor: bgColor || "",
+    display: display || "",
+    textDecoration: textDecoration || "",
+  })
+);
